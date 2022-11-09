d55517da48dd6750a3d9e52ea032d692922e36ef
Abdel Sallam <abdel@memo.cash>
2022-11-09 13:09:50 -0800

    Cleaned up logic of transfertweets

--- database/util/main.go
+++ database/util/main.go
@@ -8,7 +8,14 @@ import (
 	"github.com/memocash/tweet/database"
 	"html"
 )
-
+func updateArchiveTweetHash(archive util.Archive, ID int64, newHash []byte){
+	for i := range archive.TweetList {
+		if archive.TweetList[i].Tweet.ID == ID {
+			archive.TweetList[i].TxHash = newHash
+			break
+		}
+	}
+}
 func TransferTweets(address wallet.Address, key wallet.PrivateKey,account string, archive util.Archive, appendLink bool, appendDate bool) (int, error) {
 	var tweetList []util.TweetTx
 	//if there are at least 20 tweets not yet archived, get the oldest 20, otherwise just get all of them
@@ -23,7 +30,6 @@ func TransferTweets(address wallet.Address, key wallet.PrivateKey,account string
 		tweetList[i], tweetList[opp] = tweetList[opp], tweetList[i]
 	}
 	numTransferred := 0
-	//post each tweet that isn't a reply to some other tweet
 	for _, tweet := range tweetList {
 		tweetLink := fmt.Sprintf("\nhttps://twitter.com/twitter/status/%d\n", tweet.Tweet.ID)
 		tweetDate := fmt.Sprintf("\n%s\n", tweet.Tweet.CreatedAt)
@@ -34,52 +40,42 @@ func TransferTweets(address wallet.Address, key wallet.PrivateKey,account string
 		if appendDate {
 			tweetText += tweetDate
 		}
+		//if the tweet was a regular post, post it normally
 		if tweet.Tweet.InReplyToStatusID == 0{
 			parentHash, err := database.MakePost(address, key, html.UnescapeString(tweetText))
-			//find this tweet in archive and set its parent hash to the hash of the post that was just made
-			for i := range archive.TweetList {
-				if archive.TweetList[i].Tweet.ID == tweet.Tweet.ID {
-					archive.TweetList[i].TxHash = parentHash
-					break
-				}
-			}
+			//find this tweet in archive and set its hash to the hash of the post that was just made
+			updateArchiveTweetHash(archive, tweet.Tweet.ID, parentHash)
 			tweet.TxHash = parentHash
 			if err != nil {
 				return numTransferred,jerr.Get("error making post", err)
 			}
 		} else{
 			//find the parent tweet
-			var parentHash []byte
+			var parentHash []byte = nil
 			for _, parentTweet := range tweetList {
 				if parentTweet.Tweet.ID == tweet.Tweet.InReplyToStatusID{
 					parentHash = parentTweet.TxHash
+					break
 				}
 			}
+			//if it turns out this tweet was actually a reply to another person's tweet, post it as a regular post
 			if parentHash == nil{
 				parentHash, err := database.MakePost(address, key, html.UnescapeString(tweetText))
-				//find this tweet in archive and set its parent hash to the hash of the post that was just made
-				for i := range archive.TweetList {
-					if archive.TweetList[i].Tweet.ID == tweet.Tweet.ID {
-						archive.TweetList[i].TxHash = parentHash
-						break
-					}
-				}
+				//find this tweet in archive and set its hash to the hash of the post that was just made
+				updateArchiveTweetHash(archive, tweet.Tweet.ID, parentHash)
 				tweet.TxHash = parentHash
 				if err != nil {
 					return numTransferred,jerr.Get("error making post", err)
 				}
-			}
-			replyHash, err := database.MakeReply(parentHash,address, key, html.UnescapeString(tweetText))
-			//find this tweet in archive and set its parent hash to the hash of the post that was just made
-			for i := range archive.TweetList {
-				if archive.TweetList[i].Tweet.ID == tweet.Tweet.ID {
-					archive.TweetList[i].TxHash = replyHash
-					break
+				//otherwise, it's part of a thread, so post it as a reply to the parent tweet
+			}else{
+				replyHash, err := database.MakeReply(parentHash,address, key, html.UnescapeString(tweetText))
+				//find this tweet in archive and set its hash to the hash of the post that was just made
+				updateArchiveTweetHash(archive, tweet.Tweet.ID, replyHash)
+				tweet.TxHash = replyHash
+				if err != nil {
+					return numTransferred,jerr.Get("error making reply", err)
 				}
-			}
-			tweet.TxHash = replyHash
-			if err != nil {
-				return numTransferred,jerr.Get("error making reply", err)
 			}
 		}
 		numTransferred += 1
