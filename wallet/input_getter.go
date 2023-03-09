0fc609f01e7a5c0925ce3a185f94f3384f0f2280
Abdel Sallam <abdel@memo.cash>
2023-03-08 16:31:18 -0800

    Resolved double spending issue

--- wallet/input_getter.go
+++ wallet/input_getter.go
@@ -22,10 +22,12 @@ func (g *InputGetter) SetPkHashesToUse([][]byte) {
 }
 
 func (g *InputGetter) GetUTXOs(*memo.UTXORequest) ([]memo.UTXO, error) {
-	if g.reset && len(g.UTXOs) > 0 {
-		jlog.Logf("Using existing UTXOS: %d\n", len(g.UTXOs))
+	if g.reset {
 		g.reset = false
-		return g.UTXOs, nil
+		if len(g.UTXOs) > 0 {
+			jlog.Logf("Using existing UTXOS: %d\n", len(g.UTXOs))
+			return g.UTXOs, nil
+		}
 	}
 	client := lib.NewClient(graph.ServerUrlHttp, &Database{})
 	address := g.Address.GetAddr()
